a2af5e3cdce4d44a604d649e564da43746f9d54c
Jeff Cohen <cohen.jeff@gmail.com>
2022-10-19 23:22:09 -0500

    At the end of class on Week 4

--- week4/bitcoin.js
+++ week4/bitcoin.js
@@ -2,21 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
   const link = document.querySelector("#refresh");
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    // getBitcoinData()
+    getBitcoinData()
   });
 })
 
 // Implementation with Promise objects
-function getBitcoinData() {
-  const bitcoinUrl = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
-  let step1 = fetch(bitcoinUrl, { method: 'get' })
-  step1.then(function(response) {
-    let step2 = response.json()
-    step2.then(function(obj) {
-      document.querySelector("h2").innerText = `$ ${obj.bpi.USD.rate}`
-    })
-  })
-}
+// function getBitcoinData() {
+//   const bitcoinUrl = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+//   let step1 = fetch(bitcoinUrl, { method: 'get' })
+//   step1.then(function(response) {
+//     let step2 = response.json()
+//     step2.then(function(obj) {
+//       document.querySelector("h2").innerText = `$ ${obj.bpi.USD.rate}`
+//     })
+//   })
+// }
 
 // Alternative syntax with "arrow functions"
 // function getBitcoinData() {
@@ -31,10 +31,10 @@ function getBitcoinData() {
 // }
 
 // Alternative syntax with async/await
-// async function getBitcoinData() {
-//   const bitcoinUrl = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
-//   const httpResponse = await fetch(bitcoinUrl);
+async function getBitcoinData() {
+  const bitcoinUrl = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+  const httpResponse = await fetch(bitcoinUrl);
 
-//   const obj = await httpResponse.json()
-//   document.querySelector("h2").innerText = `$ ${obj.bpi.USD.rate}`
-// }
+  const obj = await httpResponse.json()
+  document.querySelector("h2").innerText = `$ ${obj.bpi.USD.rate}`
+}
